ec144207373dee8d493fc59f14d4449e6c04a456
PedroRomaoDev <pedro7ntj@gmail.com>
2024-11-21 23:26:07 -0300

    só falta a parte do tutu.

---
+++ View/headerHome.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/headerHome.css" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
+      rel="stylesheet"
+    />
+    <title>ACIC</title>
+  </head>
+  <body>
+    <div class="sub-header">
+        <p>
+          NOVIDADES NO ACIC SYSTEM > CONFIRA AGORA E FAÇA PARTE, NOVAS
+          FUNCIONALIDADES INOVADORAS
+        </p>
+      </div>
+    <header>
+
+      <img src="img/logo.png" alt="Logo" class="logoHeaderHome" />
+      <img src="img/perfil.png" alt="Ícone" class="icon" />
+      <img src="img/settings.png" alt="Ícone" class="icon2" />
+
+      <div class="nav-links">
+        <a class="link1" href="#">QUEM SOMOS</a>
+        <a href="#">NOSSAS POLÍTICAS</a>
+        <a href="#">NEWSLETTER</a>
+        <a href="#">SISTEMA</a>
+      </div>
+    </header>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -6,14 +6,16 @@
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="footer.css">
     <link rel="stylesheet" href="mainHome.css">
+    <link rel="stylesheet" href="headerHome.css">
     <title>ACIC</title>
 </head>
 <body>
 <?php 
-    include_once("View/header.php");
+    include_once("View/headerHome.php");
     include_once("View/mainHome.php");
     include_once("View/footer.php");
       
 ?>
+
 </body>
 </html>
